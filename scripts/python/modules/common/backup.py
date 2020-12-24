6406ea72a403ffbb6a27ff8c4153fec1bcd32079
Valentin Georgiev <diskmandata4@gmail.com>
2020-12-24 17:35:22 +0200

    update

---
+++ scripts/python/modules/common/backup.py
@@ -0,0 +1,57 @@
+'''This module implements a backup solution'''
+
+import glob
+import os
+import time
+from common.notify import notify
+
+# OK -- cd cwd - This one for sure must be set in client script.
+# lock file -- test if locking should be a method or something else...
+
+# OK -- define date var
+# OK -- move old files
+# check if host is alive
+    # allow X nums of failed attempts
+# check if remote mountpoint is there(magare)
+#     if not send alarm
+# define rsync args
+# start rsync transfer
+#     you need target-host, src, dest dirs
+# adding sync info to log files
+# cut needful info
+# add error info to summary file
+# send email
+
+class Backup:
+    def __init__(self, host):
+        self.date = time.strftime('%d_%m_%Y_%H:%M:%S')
+        self.day = time.strftime('%d')
+        self.dst_dir = '/BACKUP/' + host
+        self.log_file = 'data_' + self.day + '_' + host + '.log'
+        self.err_file = 'data_' + self.day + '_' + host + '.err'
+        self.old_files = 'old_files'
+
+        if not os.path.exists(self.dst_dir):
+            os.makedirs(self.dst_dir)
+        if not os.path.exists(self.old_files):
+            os.makedirs(self.old_files)
+
+    def cleanup(self):
+        '''move old data logs'''
+        for f in glob.glob('data_*.*'):
+            os.replace(f, self.old_files + '/' + f)
+
+    def host_check(self):
+        pass
+
+    def mount_check(self):
+        pass
+
+    def fail_bkp(self):
+        pass
+
+    def sync(self):
+        pass
+
+    def log(self):
+        pass
---
+++ scripts/python/modules/common/flocker.py
@@ -0,0 +1,36 @@
+'''Module for file locking mechanism'''
+
+import atexit
+import fcntl
+import os
+import signal
+import sys
+
+
+class FLocker:
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.file = '/run/lock/' + self.file_name + '.lock'
+
+        # clean up catchers
+        signal.signal(signal.SIGTERM, self.release)
+        signal.signal(signal.SIGINT, self.release)
+        atexit.register(self.release)
+
+        # set lock
+        try:
+            if not os.path.exists(self.file):
+                os.mknod(self.file)
+            self.f = open(self.file)
+            fcntl.flock(self.f, fcntl.LOCK_EX | fcntl.LOCK_NB)
+        except OSError as e:
+            print(e, 'SET LOCK')
+            sys.exit(1)
+
+    def release(self, signum=None, frame=None):
+        try:
+            fcntl.flock(self.f, fcntl.LOCK_UN)
+            os.remove(self.file)
+            sys.exit(1)
+        except OSError as e:
+            sys.exit(1)
